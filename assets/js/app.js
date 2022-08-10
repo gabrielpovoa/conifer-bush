b5f9b6eeb123ba30e725d7973ef7b391216fb32f
João <povoajoao6@gmail.com>
2022-08-10 08:32:03 -0300

    Slider = Checked

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,13 @@ const menu = document.getElementById('menu');
 menu.onclick = () => {
     navbar.classList.toggle('active')
 }
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        // el: ".swiper-pagination",
+        clickable: true,
+    },
+});
